0602e878309d8517ee34e138ac062e854e793da3
IAHTMY <filipammer@gmail.com>
2025-06-08 20:07:12 +0200

    Added obrazovka, font etc

--- main.py
+++ main.py
@@ -1,23 +1,16 @@
 import pygame # klíčová knihovna umožňující vytvářet jednoduše nejen hry
 pygame.init() # nutný příkaz hned na začátku pro správnou inicializaci knihovny
-"""
-class Player(pygame.sprite.Sprite):
-    def __init__(self): #konstruktor
-        super().__init__() #povolává z mrtvých konstruktor při vytvoření
-        self.image = pygame.image.load("material/protofin_23_05_25.png")
-        self.image = pygame.transform.scale(self.image, (500,600))
-        self.rect = self.image.get_rect(midbottom = (100, 0.75*window_height))
-"""
-class Button(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__()
-#       self.image = pygame.image.load("kaktus.png").convert_alpha()
-#       self.image = pygame.transform.scale(self.image,(size, size))
-#       self.rect = self.image.get_rect(bottomleft = (600, 0.75*window_height))
-#       but_1 = pygame.image.load("but_1.png")
-#       
-#       self.walking_images = [man_1, man_2, man_3, man_4]
-#       self.walking_index = 0
+
+# FONT 
+color_set = ["Black", "Blue", "Green", "Yellow", "Red", "Black"]
+color_index = 0
+if color_index == 5:
+    color_index = 0
+color = color_set[color_index]
+text_font = pygame.font.Font("material/techfont.ttf",50) # 100 je velikost písma
+text_surface = text_font.render(f"{obrazovka}", True, color) # text, anti-aliasing, černá barva písma
+text_rect = text_surface.get_rect(62, 62) # kam se to má vykreslit
+
 
 # HUDBA
 bg_music = pygame.mixer.Sound("material/music/elevator.mp3")
@@ -28,46 +21,49 @@ zelda = pygame.mixer.Sound("material/music/zelda.mp3")
 demon = pygame.mixer.Sound("material/music/demon.mp3")
 demon = pygame.mixer.Sound("material/music/demon.mp3")
 demon = pygame.mixer.Sound("material/music/demon.mp3")
-
 never = pygame.mixer.Sound("material/music/never.mp3")
 never.set_volume(0.7)
+
 fronta = []
-def calc():
+
+def calc(): # tady nevim, jak programu říct: pokud na oné pozici uvidíš znak, udělej (šlo by to vyřešit přidáním random čísel, že třeba na polozku se "znakem" 11 by se deletovalo, 10 quitla apod.)
     znak = fronta[-1]
     if znak == o:
-        pygame.quit() # zavřeme herní okno
-        exit() # úplně opustíme herní smyčku, celý program se ukončí
+        pygame.quit()
+        exit()
+
     elif znak == d:
         fronta.pop
         fronta.pop
+
     elif znak == c:
-        for i in len(fronta):
+        for _ in len(fronta):
             fronta.pop
-    elif znak == e:
-        vysledek = ()
-        for i in len(fronta):
-            vysledek.append(fronta[i]) # Tady se mu nelíbí, že nepracuju s listem. kdybych mu tam dal integer, fungovalo by to do té doby, dokud bych tam neměl znaménko +-/*...
-        print(vysledek)
-        answer == vysledek
-        # přepsání listu do (), pak se to vyprintne a na tuto hodnotu se promění i hodnota answer
+
+    elif znak == e: # tady bude problém s ukládáním proměnné i nad jedno použití této funkce (aby to answer přežilo ceký cyklus calc() bez toho, aby se znovu vymazal)
+        eval(obrazovka)
+        answer == obrazovka
+        fronta.pop
+# musím answer před použitím clac() nadefinovat, nemůžu jí ale přiřadit číslo -> po spuštění by se hned mohlo použít i bez předchozího výpočtu (například 0)
+# musím zavést globální proměnnou, která se bude moci menit skrz používání programu a zároveň na začátku nebude mít hodnotu (nebude definovaná)
+        if obrazovka == 2:
+            color += 1
+        if obrazovka == 1953:    # department eastereggů
+            doppler.play()
+        if obrazovka == 58008:
+            zelda.play()
+        if obrazovka == 666:
+            demon.play()
+        if obrazovka == 1987:
+            never.play()
+        if obrazovka == 215396891: #solar panel easter egg, čislo 215396891, třeba zpusobem exec(celá dinogame)
+            pass
+
     elif znak == a:
         fronta.append(answer)
-    
-
-    if vysledek == 1953:    # department eastereggů
-        doppler.play()
-    if vysledek == 58008:
-        zelda.play()
-    if vysledek == 666:
-        demon.play()
-    if vysledek == něco:
-        pass # zde by se zpustilo minigame dinogame, pojmenovat podle Tuky
-
-
-
-
-    if vysledek == 215396891: #solar panel easter egg, čislo 215396891
-        never.play()
+        fronta.pop
+    obrazovka = "".join(fronta)
+    print(obrazovka)
 
 names = ["1b.png", "2b.png", "3b.png", "4b.png", "5b.png", "6b.png", "7b.png", "8b.png", "9b.png", "10b.png", 
         "11b.png", "12b.png", "13b.png", "14b.png", "15b.png", "16b.png", "17b.png", "18b.png", "19b.png", "20b.png", 
@@ -76,39 +72,30 @@ functions = ["**(1/2)", "**2", " ", "o", " ", "7", "8", "9", "d", "c",
              "4", "5", "6", "*", "/", "1", "2", "3", "+", "-", 
              "0", "00", " ", "a", "e"]
 
-eval(functions)
 # o = off, d = delete, c = all clear, a = answer, e = execute (=)
 
 for i in range (0, 24):
         buttons_surface = pygame.image.load(f"material/buttons/{names[i]}")
-x = 40
-y = 250
+x = 32
+y = 308
 buttons_group = pygame.sprite.Group()
+mouse = pygame.mouse.get_pos()
+
 def is_collision():
-    if pygame.sprite.spritecollide(mouse, buttons_group, False): # False na konci určuje, zda-li má kolidující obstacle zabít
+    if pygame.sprite.spritecollide(mouse, buttons_group, False):
         fronta.append(functions[names.index(buttons_group)])
         calc()
+# zde je problém, který se vyřeší poté, co se inteligentně načtou všechny buttons
+# jak zjistim, se kterým rect to koliduje, abych mohl do fronty appendnout adekvátní znak
 
 window_width = 500
 window_height = 600
 
 calculator_surface = pygame.image.load("material/protofin_25_05_25.png")
-calculator_surface = pygame.transform.scale(calculator_surface, (500,600))
-"""
-text_font = pygame.font.Font("PixelifySans.ttf",100) # 100 je velikost písma
-text_surface = text_font.render("GAME OVER!", True, "Black") # text, anti-aliasing, černá barva písma
-text_rect = text_surface.get_rect(20, 20) # kam se to má vykreslit
-"""
+calculator_surface = pygame.transform.scale(calculator_surface, (window_width, window_height))
+
 screen = pygame.display.set_mode((window_width, window_height))
 clock = pygame.time.Clock()
-
-print(eval(""))
-
-exec("""
-a = 7
-b = 8
-print("vysledek: ", a*b)
-""")
 
 while True:
 
@@ -122,20 +109,15 @@ while True:
             is_collision()
             
     screen.blit(calculator_surface,(0,0)) # položíme pozadí na souřadnice [0,0]
-    for j in range (1, 5):
-        for i in range (0, 24):
-            buttons_surface.blit(names[i], (x,y))
-        x += 84
-    x == 40
-    y += 52
-    buttons_surface.blit()
-    mouse = pygame.mouse.get_pos()
-    print(mouse)
+
+    for i in range (0, 24):
+            for l in range(5):
+                for j in range(5):
+                    buttons_surface.blit(names[i], (x,y))
+                x += 88
+            x == 32
+            y += 56
     
-
-# pokud klkiknu na tlačítko, ať se appendne indexem daný znak a pak se spustí funkce kalkulačky, která bude kontrolovat poslední část fronty
-
-
     pygame.display.update() # updatujeme vykreslené okno
     clock.tick(5) # herní smyčka proběhne maximálně 5x za sekundu
 
