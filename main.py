19810f9571ef4956f6f7139a614fa64710d81673
IAHTMY <filipammer@gmail.com>
2025-06-10 20:23:58 +0200

    Fixed some stuff, added dino, icons

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import pygame # klíčová knihovna umožňující vytvářet jednoduše nejen hry
 pygame.init() # nutný příkaz hned na začátku pro správnou inicializaci knihovny
 
+fronta = []
+obrazovka = None
+answer = None
+
+# IKONA
+calcicon = pygame.image.load("material/calcicon.png")  # Make sure the image is in the same directory
+pygame.display.set_icon(calcicon)
+
+# JMENO HRY
+pygame.display.set_caption("Kalkulačka Gasio 123")
+
 # FONT 
 color_set = ["Black", "Blue", "Green", "Yellow", "Red", "Black"]
 color_index = 0
@@ -10,7 +21,6 @@ color = color_set[color_index]
 text_font = pygame.font.Font("material/techfont.ttf",50) # 100 je velikost písma
 text_surface = text_font.render(f"{obrazovka}", True, color) # text, anti-aliasing, černá barva písma
 text_rect = text_surface.get_rect(62, 62) # kam se to má vykreslit
-
 
 # HUDBA
 bg_music = pygame.mixer.Sound("material/music/elevator.mp3")
@@ -24,28 +34,25 @@ demon = pygame.mixer.Sound("material/music/demon.mp3")
 never = pygame.mixer.Sound("material/music/never.mp3")
 never.set_volume(0.7)
 
-fronta = []
-
 def calc(): # tady nevim, jak programu říct: pokud na oné pozici uvidíš znak, udělej (šlo by to vyřešit přidáním random čísel, že třeba na polozku se "znakem" 11 by se deletovalo, 10 quitla apod.)
     znak = fronta[-1]
-    if znak == o:
+    if znak == "o":
         pygame.quit()
         exit()
 
-    elif znak == d:
+    elif znak == "d":
         fronta.pop
         fronta.pop
 
-    elif znak == c:
-        for _ in len(fronta):
+    elif znak == "c":
+        for _ in range(len(fronta)):
             fronta.pop
 
-    elif znak == e: # tady bude problém s ukládáním proměnné i nad jedno použití této funkce (aby to answer přežilo ceký cyklus calc() bez toho, aby se znovu vymazal)
+    elif znak == "e": # tady bude problém s ukládáním proměnné i nad jedno použití této funkce (aby to answer přežilo ceký cyklus calc() bez toho, aby se znovu vymazal)
         eval(obrazovka)
-        answer == obrazovka
+        answer = obrazovka
         fronta.pop
-# musím answer před použitím clac() nadefinovat, nemůžu jí ale přiřadit číslo -> po spuštění by se hned mohlo použít i bez předchozího výpočtu (například 0)
-# musím zavést globální proměnnou, která se bude moci menit skrz používání programu a zároveň na začátku nebude mít hodnotu (nebude definovaná)
+
         if obrazovka == 2:
             color += 1
         if obrazovka == 1953:    # department eastereggů
@@ -57,11 +64,164 @@ def calc(): # tady nevim, jak programu říct: pokud na oné pozici uvidíš zna
         if obrazovka == 1987:
             never.play()
         if obrazovka == 215396891: #solar panel easter egg, čislo 215396891, třeba zpusobem exec(celá dinogame)
-            pass
+            
+            dino ="""pygame.init() # nutný příkaz hned na začátku pro správnou inicializaci knihovny
+                import random
+                class Player(pygame.sprite.Sprite): 
+                    def __init__(self):
+                        super().__init__()
+                        man_1 = pygame.image.load("material/dino/man_1.png")
+                        man_2 = pygame.image.load("material/dino/man_2.png")
+                        man_3 = pygame.image.load("material/dino/man_3.png")
+                        man_4 = pygame.image.load("material/dino/man_4.png")
+                        self.walking_images = [man_1, man_2, man_3, man_4]
+                        self.walking_index = 0
+                        self.jump_sound = pygame.mixer.Sound("material/dino/jump.mp3")
+                        self.jump_sound.set_volume(0.2)
+                        self.image = self.walking_images[self.walking_index]
+                        self.rect = self.image.get_rect(bottomleft=(100,0.75*window_height))
+                        self.gravity = 0
 
-    elif znak == a:
+                    def player_input(self):
+                        keys = pygame.key.get_pressed()
+                        if keys[pygame.K_SPACE] and self.rect.bottom >= 300:
+                            self.gravity = -20
+                            self.jump_sound.play()
+                    def apply_gravity(self):
+                        self.gravity += 1
+                        self.rect.y += self.gravity
+                        if self.rect.bottom >= 300:
+                            self.rect.bottom = 300
+
+                    def animation_state(self):
+                        self.walking_index += 0.1
+                        if self.walking_index >= 4:
+                            self.walking_index = 0
+                        self.image = self.walking_images[int(self.walking_index)]
+
+                    def update(self):
+                        self.player_input()
+                        self.apply_gravity()
+                        self.animation_state()
+
+                    
+                class Obstacle(pygame.sprite.Sprite):
+                    def __init__(self):
+                        super().__init__()
+                        self.image = pygame.image.load("material/dino/kaktus.png").convert_alpha()
+                        size = random.randint(30,80)
+                        self.image = pygame.transform.scale(self.image,(size, size))
+                        self.rect = self.image.get_rect(bottomleft = (600, 0.75*window_height))
+                        self.speed  = 10
+                        
+                    def update(self):
+                        self.rect.x -= 6
+                        self.destroy()
+                        
+                    def destroy(self):
+                        if self.rect.x <= -100: 
+                            self.kill()
+
+                def is_collision():
+                    if pygame.sprite.spritecollide(player.sprite, obstacle_group, False): # False na konci určuje, zda-li má kolidující obstacle zabít
+                        obstacle_group.empty()
+                        return False
+                    return True
+
+                # herní okno
+                window_width = 800
+                window_height = 400
+                screen = pygame.display.set_mode((window_width, window_height))
+                    # dvojice (w,h) v parametru se nazývá *tuple*
+                pygame.display.set_caption("Dinosauří hra") # nastavíme do hlavičky okna název hry
+                
+                icondino = pygame.image.load("material/dino/dinoicon.png")  # Make sure the image is in the same directory
+                pygame.display.set_icon(icondino)
+                
+                clock = pygame.time.Clock() # díky hodinám nastavíme frekvenci obnovování herního okna
+
+                # přidání objektů (tzv. surface) do scény
+                sky_surface = pygame.Surface((window_width,0.75*window_height))
+                sky_surface.fill("darkslategray1")
+                ground_surface = pygame.Surface((window_width,0.25*window_height))
+                ground_surface.fill("lightsalmon4")
+
+                # text
+                text_font = pygame.font.Font("material/dino/PixelifySans.ttf",100)
+                text_surface = text_font.render("GAME OVER!", True, "Black")
+                text_rect = text_surface.get_rect(center=(window_width/2, window_height/2))
+
+                score_font = pygame.font.Font("material/dino/PixelifySans.ttf",100) # 100 je velikost písma
+
+                # Groups
+                # nepřátelé
+                obstacle_group = pygame.sprite.Group()
+
+                # hráč
+                player = pygame.sprite.GroupSingle()
+                player.add(Player())
+
+                game_active = is_collision()
+
+                counter = 110
+                bg_music = pygame.mixer.Sound("material/dino/music.mp3")
+                bg_music.play(loops = -1)
+
+                count = 0
+
+                # herní smyčka
+                while True:
+                    # zjistíme co dělá hráč za akci
+                    for event in pygame.event.get():
+                        if event.type == pygame.QUIT:
+                            pygame.quit() # zavřeme herní okno
+                            exit() # úplně opustíme herní smyčku, celý program se ukončí
+                        if event.type == pygame.KEYDOWN:
+                            if game_active == False:
+                                game_active=True
+                                bg_music.play()
+
+                    if game_active:
+                        # pozadí
+                        screen.blit(sky_surface,(0,0)) # položíme sky_surface na souřadnice [0,0]
+                        screen.blit(ground_surface,(0,0.75*window_height)) # položíme ground_surface na souřadnice [0,300] (pod oblohu)
+
+                        if counter > 120:
+                            counter = 0
+                            obstacle_group.add(Obstacle())
+                            
+                        else:
+                            counter += 1
+
+                        # nepřítel
+                        obstacle_group.draw(screen)
+                        obstacle_group.update()
+
+                        # hráč
+                        player.draw(screen)
+                        player.update()
+                        
+                        count += 1
+                        score_surface = text_font.render(f"Skore: {count // 60}", True, "Black") # text, anti-aliasing, černá barva písma
+                        score_rect = text_surface.get_rect(center=(window_width/2, window_height/2)) # kam se to má vykreslit
+                        screen.blit(score_surface, score_rect)
+                        
+                        game_active = is_collision()
+
+                    else:
+                        pygame.draw.rect(screen, (255,0,0), pygame.Rect(window_width*0.15, window_height*0.25, window_width*0.7, window_height*0.5))
+                        count = 0
+                        screen.blit(text_surface, text_rect)
+                        bg_music.stop()
+
+                    pygame.display.update() # updatujeme vykreslené okno
+                    clock.tick(60)"""
+            exec(dino)
+
+    elif znak == "a":
         fronta.append(answer)
         fronta.pop
+
     obrazovka = "".join(fronta)
     print(obrazovka)
 
@@ -125,10 +285,6 @@ while True:
 
 # TODO list
 #   vytvorit group tlačítek
-#   připravit hudbu
 #   rozmyšlení animace tlačítek
 #   vyresit ukazatel postupu
-
-#   parapetry tlačítek 39*23 (stará verze někdy i 41*23)
-#   užitečné
 #   https://www.pygame.org/docs/py-modindex.html
